8458f3dd6d7e16a59a4b7faab97a10977cf8dc90
dmerskine19 <81436471+dmerskine19@users.noreply.github.com>
2024-05-27 13:49:11 -0400

    enabled image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,6 @@ import path from 'path';
 
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
   webpack: (config, { isServer }) => {
     // Add CSS support for CSS modules
     config.module.rules.push({
