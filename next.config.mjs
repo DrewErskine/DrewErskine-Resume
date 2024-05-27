e9fa5f2d29c4e43066189b82eb27088337fe1958
dmerskine19 <81436471+dmerskine19@users.noreply.github.com>
2024-05-27 10:33:42 -0400

    Update configuration for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/dmerskine19.github.io',
+  assetPrefix: '/dmerskine19.github.io/',
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Example webpack custom configuration
     return config;
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
