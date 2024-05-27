512d5aea40915fc11b91cdaef849a8a738fc6012
dmerskine19 <81436471+dmerskine19@users.noreply.github.com>
2024-05-27 12:59:40 -0400

    styleLoader

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,19 @@
-/** @type {import('next').NextConfig} */
+// next.config.mjs
+import path from 'path';
+
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  pageExtensions: ['tsx', 'ts', 'jsx', 'js'], // Ensure all necessary extensions are included
-  images: {
-    unoptimized: true, // Disable image optimization for static export
-  },
-  experimental: {
-    appDir: true, // Enable the App Directory feature
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/dmerskine19.github.io' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/dmerskine19.github.io/' : '',
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Example webpack custom configuration
+  webpack: (config, { isServer }) => {
+    // Add CSS support for CSS modules
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'postcss-loader',
+      ],
+    });
+
     return config;
   },
 };
