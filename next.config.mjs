f38495e8f762567650263ec4bdcc701c05914fde
dmerskine19 <81436471+dmerskine19@users.noreply.github.com>
2024-05-27 18:34:19 -0400

    removed css custom imports in next config lets go

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,11 @@
 // next.config.mjs
-import path from 'path';
-
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: false, // Ensure images are optimized by Next.js
   },
   webpack: (config, { isServer }) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'postcss-loader',
-      ],
-    });
-
+    // Custom webpack configurations can go here if needed, but avoid custom CSS loaders
     return config;
   },
 };
