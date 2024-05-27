3cfdcea76768edcda17fda7836e416dfcc98b488
dmerskine19 <81436471+dmerskine19@users.noreply.github.com>
2024-05-27 11:00:28 -0400

    run dev working

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/dmerskine19.github.io',
-  assetPrefix: '/dmerskine19.github.io/',
+  output: 'export',
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'], // Ensure all necessary extensions are included
+  images: {
+    unoptimized: true, // Disable image optimization for static export
+  },
+  experimental: {
+    appDir: true, // Enable the App Directory feature
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/dmerskine19.github.io' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/dmerskine19.github.io/' : '',
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Example webpack custom configuration
     return config;
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,14 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import '../../styles/globals.css';
+// src/app/layout.tsx
+import { ReactNode } from 'react';
 
-const inter = Inter({ subsets: ['latin'] });
-
-export const metadata: Metadata = {
-  title: 'Drew Erskine - Portfolio',
-  description: 'A portfolio showcasing my skills and experience in software development.',
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content='A portfolio showcasing my skills and experience in software development.' />
-        <link rel="icon" href="/favicon.ico" />
-      </head>
-      <body className={`${inter.className} bg-deep-space text-white`}>
+      <body>
         {children}
       </body>
     </html>
   );
-}
+};
+
+export default RootLayout;
