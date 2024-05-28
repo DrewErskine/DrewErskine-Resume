67e3ebd003897a49b24dfbceb0814f2486b79211
dmerskine19 <81436471+dmerskine19@users.noreply.github.com>
2024-05-28 18:11:33 -0400

    ResumeV3

--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -1,16 +1,20 @@
 import React from "react";
+import "../styles/creds.css";
 
 const Certifications = () => (
   <section className="section half-section certifications-section" id="certifications">
     <h2 className="section-title">Certifications</h2>
     <ul className="certifications-text">
-      <li>Certification A</li>
+      <li>
+        <h3>Certification A</h3>
+        <p>Info on Certification A</p>
+      </li>
     </ul>
     <ul className="certifications-text">
-      <li>Certification B</li>
-    </ul>
-    <ul className="certifications-text">
-      <li>Certification C</li>
+      <li>
+        <h3>Certification B</h3>
+        <p>Info on Certification B</p>
+      </li>
     </ul>
   </section>
 );
