d9beaff75951e47567254c2c94a5a40a6ee9b64c
dmerskine19 <81436471+dmerskine19@users.noreply.github.com>
2024-05-30 19:45:43 -0400

    ResumeV4.0.1

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,4 @@
-import '../styles/carousel.css'; // Assuming you are using the same CSS file
-
+import "../styles/carousel.css"; // Assuming you are using the same CSS file
 
 interface ProjectsProps {
   currentProjectIndex: number;
@@ -8,19 +7,24 @@ interface ProjectsProps {
 const projectDetails = [
   {
     title: "Peep-Pea Blog Platform",
-    description: "A robust blogging platform built with Spring Boot, demonstrating modern Java development practices with a RESTful API, ensuring scalable and reliable content management.",
-    technologies: "SpringBoot 3, React, PostgreSQL, H2, MySQL, OAuth2, CRUD Operations"
+    description:
+      "A robust blogging platform built with Spring Boot, demonstrating modern Java development practices with a RESTful API, ensuring scalable and reliable content management.",
+    technologies:
+      "SpringBoot 3, React, PostgreSQL, H2, MySQL, OAuth2, CRUD Operations",
   },
   {
     title: "Pokedex Explorer - https://drew-erskine.web.app/pokemon-search",
-    description: "A dynamic Pokedex application built with Node.js and Vue.js, featuring a real-time, interactive interface for exploring Pokémon, items, and moves, supporting user experience with both a CLI and web-based interfaces.",
-    technologies: "Node.js, Vue.js, Vite, Tailwind CSS, HTML, Firebase"
+    description:
+      "A dynamic Pokedex application built with Node.js and Vue.js, featuring a real-time, interactive interface for exploring Pokémon, items, and moves, supporting user experience with both a CLI and web-based interfaces.",
+    technologies: "Node.js, Vue.js, Vite, Tailwind CSS, HTML, Firebase",
   },
   {
     title: "HexWrld",
-    description: "HexWrld is a dynamic Blender Geometry Nodes animation that exemplifies procedural generation and graphical computing. The project features a landscape made of hexagons and 2D grass, enhanced with vibrant lighting effects, demonstrating the capabilities of procedural content generation and the power of computer graphics for creating engaging visual environments.",
-    technologies: "Blender, Geometry Nodes, Shaders, Drivers, Value Keying, HTML, CSS, JavaScript, Firebase"
-  }
+    description:
+      "HexWrld is a dynamic Blender Geometry Nodes animation that exemplifies procedural generation and graphical computing. The project features a landscape made of hexagons and 2D grass, enhanced with vibrant lighting effects, demonstrating the capabilities of procedural content generation and the power of computer graphics for creating engaging visual environments.",
+    technologies:
+      "Blender, Geometry Nodes, Shaders, Drivers, Value Keying, HTML, CSS, JavaScript, Firebase",
+  },
 ];
 
 function Projects({ currentProjectIndex }: ProjectsProps) {
